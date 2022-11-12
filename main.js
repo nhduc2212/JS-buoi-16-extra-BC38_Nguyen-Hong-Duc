a7ad8ce63f306b1b1fa0ad04a28fc31ab009aa18
Duc Nguyen <koutoku2212@gmail.com>
2022-11-12 22:14:57 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function ex5() {
+  var number = document.getElementById("ex5-input-number").value * 1;
+  var resultArray=[];
+  
+  var k = 2;
+  for (var i = 1; i <= number; i++) {
+    var notPrimeNumberCount = 0;
+    for (k = 1; k <= i; k++) {
+      if (i % k === 0) {
+        notPrimeNumberCount++;
+      }
+    }
+    if (notPrimeNumberCount === 2) {
+        resultArray.push(i);
+      }
+  }
+  document.getElementById("ex5-result").innerHTML = resultArray;
+}
